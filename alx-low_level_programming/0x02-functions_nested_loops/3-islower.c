a1a4197b8ece948973f74560228ed0690c9fc68f
jwd-katten <jwd.katten@gmail.com>
2023-06-20 19:40:33 +0100

    complete task 3 lower case

---
+++ alx-low_level_programming/0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower -  check if the caratcter is lowercase
+ * @c: The character to be checked.
+ *
+ *
+ * Return: Always 0 (Success)
+*/
+
+int _islower(int c);
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
