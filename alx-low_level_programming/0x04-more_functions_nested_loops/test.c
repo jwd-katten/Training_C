9b24e1da8e8bd4d30832a3d48849d6926306bab4
jwd-katten <jwd.katten@gmail.com>
2023-06-22 14:12:27 +0100

    cmplete 0-isupper

---
+++ alx-low_level_programming/0x04-more_functions_nested_loops/test.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%d \n", c);
+	c = 'Z';
+	printf("%d: \n", c);
+	return (0);
+}
