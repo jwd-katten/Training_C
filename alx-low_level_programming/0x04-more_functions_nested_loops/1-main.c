79b8319a83e18aff5050fbe983f2e5cc069fb564
jwd-katten <jwd.katten@gmail.com>
2023-06-22 14:34:20 +0100

    task two complete 1-isdigit

--- alx-low_level_programming/0x04-more_functions_nested_loops/1-main.c
+++ alx-low_level_programming/0x04-more_functions_nested_loops/1-main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
@@ -11,8 +11,8 @@ int main(void)
 	char c;
 
 	c = '0';
-	printf("%d \n", c);
-	c = '9';
-	printf("%d: \n", c);
+	printf("%c: %d\n", c, _isdigit(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isdigit(c));
 	return (0);
 }
